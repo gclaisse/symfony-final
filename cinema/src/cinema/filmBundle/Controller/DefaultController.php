fba39c72c59ae2c4eece21df3a5d5e4ba9e248c2
Unknown <guillaumeclaisse@yahoo.fr>
2017-01-31 09:51:08 +0100

    add filmBundle

---
+++ cinema/src/cinema/filmBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace cinema\filmBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('cinemafilmBundle:Default:index.html.twig');
+    }
+}
---
+++ cinema/src/cinema/filmBundle/cinemafilmBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace cinema\filmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class cinemafilmBundle extends Bundle
+{
+}
